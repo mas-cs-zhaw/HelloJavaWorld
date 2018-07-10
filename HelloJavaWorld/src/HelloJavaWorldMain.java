db30bfffb86d2f6d0af3c4b4cab04114554f751e
mas-cs-zhaw <40994622+mas-cs-zhaw@users.noreply.github.com>
2018-07-10 08:07:10 +0200

    Wind 10 Commit

--- HelloJavaWorld/src/HelloJavaWorldMain.java
+++ HelloJavaWorld/src/HelloJavaWorldMain.java
@@ -4,7 +4,7 @@ public class HelloJavaWorldMain {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World");
 		// comment macbook
-		// comment Win
+		// comment Win 10
 	}
 
 }
